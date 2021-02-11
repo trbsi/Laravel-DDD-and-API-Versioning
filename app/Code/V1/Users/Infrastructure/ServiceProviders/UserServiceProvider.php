1604c6c2480c91f48d4ef6711cd9edf82724e9ee
Dario Trbovic <timmy.dario@gmail.com>
2021-02-11 18:53:18 +0100

    Add user service provider

---
+++ app/Code/V1/Users/Infrastructure/ServiceProviders/UserServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Code\V1\Users\Infrastructure\ServiceProviders;
+
+use App\Code\V1\Users\Domain\CreateUser\Interfaces\SaveUserInterface;
+use App\Code\V1\Users\Domain\CreateUser\Interfaces\SendEmailInterface;
+use App\Code\V1\Users\Infrastructure\CreateUser\Services\SaveUserToDatabase;
+use App\Code\V1\Users\Infrastructure\CreateUser\Services\SendEmailToUser;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(SaveUserInterface::class, SaveUserToDatabase::class);
+        $this->app->bind(SendEmailInterface::class, SendEmailToUser::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
