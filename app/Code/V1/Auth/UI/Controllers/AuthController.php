f91e94abe12698fcf2305c6ae92fcffa095926ec
Dario Trbovic <timmy.dario@gmail.com>
2021-02-11 18:49:50 +0100

    Add CreateTokenRequest

--- app/Code/V1/Auth/UI/Controllers/AuthController.php
+++ app/Code/V1/Auth/UI/Controllers/AuthController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\Code\V1\Auth\UI\Controllers;
 
 use App\Code\V1\Auth\Application\Middlemen\CreateTokenMiddleman;
+use App\Code\V1\Auth\Infrastructure\CreateToken\Services\CreateTokenService;
+use App\Code\V1\Auth\UI\Requests\CreateTokenRequest;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
@@ -11,14 +13,9 @@ use Illuminate\Validation\ValidationException;
 
 final class AuthController
 {
-    public function createToken(Request $request, CreateTokenMiddleman $createTokenMiddleman)
+    public function createToken(CreateTokenRequest $request, CreateTokenMiddleman $createTokenMiddleman)
     {
         try  {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required',
-            ]);
-
             $createTokenMiddleman->createToken($request->email, $request->password);
         } catch (Exception $e) {
             abort($e->getCode(), $e->getMessage());
